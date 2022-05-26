c3f0ac371758df0b7c8bc252be4630e1b4a352ea
Joseph Hill <joseph.hill1@outlook.com>
2022-05-25 21:36:05 -0400

    Added health check class

---
+++ ICMPHealthCheck.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.NetworkInformation;
+namespace HealthCheckAPI
+{
+  public class ICMPHealthCheck : IHealthCheck
+  {
+    private readonly string Host = $"10.0.0.0";
+    private readonly int HealthyRoundtripTime = 300;
+    public async Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+      try
+      {
+        using var ping = new Ping();
+        var reply = await ping.SendPingAsync(Host);
+        switch (reply.Status)
+        {
+          case IPStatus.Success:
+            return (reply.RoundtripTime > HealthyRoundtripTime)
+                ? HealthCheckResult.Degraded()
+                : HealthCheckResult.Healthy();
+          default:
+            return HealthCheckResult.Unhealthy();
+        }
+      }
+      catch (Exception e)
+      {
+        return HealthCheckResult.Unhealthy();
+      }
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+global using HealthCheckAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHealthChecks().AddCheck<ICMPHealthCheck>("ICMP");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +21,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseHealthChecks(new PathString("/api/health"));
 
 app.MapControllers();
 
